9c23286733f2f03f028094a0708a94d266fbcbeb
Jang-Jaewon <jewon119@gmail.com>
2022-03-27 12:59:01 +0900

    Add: Viewset을 활용하여 Post API 구현

--- api/views.py
+++ api/views.py
@@ -2,9 +2,15 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer, PostSerializer
+from blog.models import Post
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
